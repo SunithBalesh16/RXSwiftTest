078312c2745861195be7ff8b56c97f71ad155075
Sunith <sunith.balesh@gmail.com>
2019-09-04 17:51:43 +0530

    Review changes(https://github.com/SunithBalesh16/RXSwiftTest/pull/1#discussion_r320256196,https://github.com/SunithBalesh16/RXSwiftTest/pull/1#discussion_r320254843), add a transaction, added loader,

--- RXSwiftTest/AddTransaction/AddTransactionVC.swift
+++ RXSwiftTest/AddTransaction/AddTransactionVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ReactiveSwift
 
 protocol AddTransactionDelegate : class {
     
@@ -17,70 +18,87 @@ protocol AddTransactionDelegate : class {
 class AddTransactionVC: UIViewController {
     
     //MARK: - IBOutlets
-    @IBOutlet weak var descriptionTextView: UITextView! {
-        didSet {
-            //            descriptionTextView.inputAccessoryView = Helper.getToolBar(selector: #selector(AddTransactionVC.dismissKeyboard), viewController: self, title: "Done")
-        }
-    }
+    @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var balanceLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
-    @IBOutlet weak var amountTextField: UITextField! {
-        didSet {
-            //            amountTextField.inputAccessoryView = Helper.getToolBar(selector: #selector(AddTransactionVC.dismissKeyboard), viewController: self, title: "Done")
-        }
-    }
+    @IBOutlet weak var amountTextField: UITextField!
     
     //MARK: - Instance Members
-    private var user = User.shared.getCurrentUser()
+    private var user = User.shared()
     weak var delegate : AddTransactionDelegate?
+    private var addTransactionSignal: SignalProducer<Data, RXTestError>?
+    private var inProgressSignal: Disposable?
+    private lazy var loadingVC = LoadingVC()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Helper.addShadow(forLayer: headerView.layer, opacity: 0.5, offset: CGSize(width: 0, height: 3.0), radius: 2, color: UIColor.darkGray)
-        balanceLabel.text = "Balance : " + user.balance
+        setupViews()
         // Do any additional setup after loading the view.
     }
     
     //MARK: - User Interactions
-    @IBAction func spend(_ sender: Any) {
+    @IBAction func spend(_ sender: UIButton) {
         
-        if amountTextField.text! == "" {
+        guard let amountText = amountTextField.text else {return}
+        guard let description = descriptionTextView.text else {return}
+        
+        if amountText.isEmpty {
             Helper.showAlert(viewController: self, title: "Please enter valid amount")
             return
         }
         
-        if descriptionTextView.text! == "" {
+        if description.isEmpty {
             Helper.showAlert(viewController: self, title: "Please enter valid description")
             return
         }
         
-        guard let amount = Float(amountTextField.text!) , let balance = Float(user.balance) else { return }
+        guard let amount = Float(amountText), let balance = Float(user.balance) else { return }
         if amount > balance {
             Helper.showAlert(viewController: self, title: "You have exceeded available balance")
             return
         }
-        
-        let date = Helper.getTimeFromDate(dateString: "\(Date())", currentDateFormat: "yyyy-MM-dd HH:mm:ss ZZZ", requiredDateFormat: "yyyy-MM-dd'T'HH:mm:ssZ")
-        let requestDict : [String:Any] = ["date": date,"description": descriptionTextView.text!,"amount": amountTextField.text!,"currency":user.currency]
-        activityIndicator.startAnimating()
-        
-//        APIHandler.shared.makeTransaction(requestDict: )
+        add(loadingVC)
+        addTransactionSignal = APIHandler.shared.makeTransaction(transaction: Transaction(desc: description, amount: amountText))
+        observeChanges()
         
     }
     
     @IBAction func back(_ sender: Any) {
-        
         self.navigationController?.popViewController(animated: true)
-        
     }
     
     //MARK: - Private Methods
+    private func setupViews() {
+        Helper.addShadow(forLayer: headerView.layer, opacity: 0.5, offset: CGSize(width: 0, height: 3.0), radius: 2, color: UIColor.darkGray)
+        balanceLabel.text = "Balance : " + user.balance
+    }
+    
     private func dismissKeyboard() {
-        
         amountTextField.resignFirstResponder()
         descriptionTextView.resignFirstResponder()
-        
+    }
+    
+    private func observeChanges() {
+        guard let signal = addTransactionSignal else {
+            return
+        }
+        inProgressSignal?.dispose()
+        inProgressSignal = signal.observe(on: UIScheduler()).on(event: { [weak self] (event) in
+            guard let `self` = self else { return }
+            self.loadingVC.remove()
+            switch event {
+            case .value(_):
+                guard let delegate = self.delegate else { return }
+                delegate.didAddTransaction()
+                self.navigationController?.popViewController(animated: true)
+                break
+            case .failed(let error):
+                self.showAlert(title: "", message: error.localizedDescription)
+            default:
+                break
+            }
+        }).start()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -102,7 +120,7 @@ extension AddTransactionVC : UITextViewDelegate , UITextFieldDelegate {
         let numberOfDots = newText.components(separatedBy: ".").count - 1
         
         let numberOfDecimalDigits: Int
-        if let dotIndex = newText.index(of: ".") {
+        if let dotIndex = newText.firstIndex(of: ".") {
             numberOfDecimalDigits = newText.distance(from: dotIndex, to: newText.endIndex) - 1
         } else {
             numberOfDecimalDigits = 0
@@ -123,3 +141,5 @@ extension AddTransactionVC : UITextViewDelegate , UITextFieldDelegate {
     
     
 }
+
+
